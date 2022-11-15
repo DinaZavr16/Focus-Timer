3c3d7f2ba52e32c3fbaced663f1b2b4c600c58e4
Dina <dina.formakidov@limestonedigital.com>
2022-11-15 19:23:46 +0100

    - basic implementation

---
+++ src/features/AboutMe.js
@@ -0,0 +1,54 @@
+import React, { useContext } from "react";
+import { Avatar, List, Button } from "react-native-paper";
+import { SafeArea } from "../../../components/utility/safe-area.component";
+import styled from "styled-components";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+
+const AuthorInfo = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.title};
+`;
+
+const AuthorInfoContainer = styled.View`
+  flex: 0.4;
+  align-items: center;
+`;
+
+const AuthorPhotoContainer = styled.View`
+  flex: 0.6;
+  align-items: center;
+  justify-content: center;
+`;
+
+// const List.Section = styled(List.Item)`
+//   padding: ${(props) => props.theme.space[3]};
+// `;
+
+export const AboutAuthorScreen = ({ navigation }) => {
+  const { onLogout, user } = useContext(AuthenticationContext);
+
+  return (
+    <SafeArea>
+      <AuthorPhotoContainer>
+        <Avatar.Image
+          size={250}
+          source={{
+            uri: "https://ibb.co/cNwyVj1",
+          }}
+        />
+      </AuthorPhotoContainer>
+      <AuthorInfoContainer>
+        <AuthorInfo>Developer: Dina Formakidov</AuthorInfo>
+        <AuthorInfo>Company: LimeStone Digital</AuthorInfo>
+      </AuthorInfoContainer>
+      <Button
+        icon="logout"
+        mode="contained"
+        onPress={onLogout}
+        buttonColor="red"
+      >
+        Logout
+      </Button>
+    </SafeArea>
+  );
+};
